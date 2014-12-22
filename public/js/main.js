ee5d1aebc37c19f56126f8ecc68b9908b29a1878
jagdish adusumalli <jagdish.adusumalli@gmail.com>
2014-12-22 21:37:20 +0530

    jaya gupta changes

--- public/js/main.js
+++ public/js/main.js
@@ -18,7 +18,7 @@ jQuery(function($) {
 	//Ajax contact
 	$('#submit-form').click(function () {
     $.ajax({
-      url: "//forms.brace.io/info@genencore.com",
+      url: "//formspree.io/info@genencore.com",
       method: "POST",
       data: {
               message: $("#message").val(),
